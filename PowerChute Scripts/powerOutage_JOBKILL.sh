50a4b89d3581f2edb60cd1b7b7b66b7c633bd493
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-10-17 22:11:32 -0700

    Add checking for job to cancel. Then initiating shutdown.

--- PowerChute Scripts/powerOutage_JOBKILL.sh
+++ PowerChute Scripts/powerOutage_JOBKILL.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 job=$(squeue -h | wc -l)
 t=$(date)
-
+count=1
 if [ $job -gt 0 ] 
-then
-    echo "${t} Loss of power. Shutting down." > ABORT
+    then
+        echo "${t} Loss of power. Shutting down." > ABORT
 
 fi 
 
+while [ $job -gt 0] && [ $count -lt 11 ]
+    do 
+        job=$(squeue -h | wc -l)
+        ((count+=1))
+        sleep 60
+    done
+if [ $job -lt 0 ]
+    then 
+        shutdown now
+else
+    jobID=$(squeue -h | awk {{print $1}})
+    sudo scancel $jobID
+    sleep 30
+    shutdown now
