b159a8bfe3e3aa6c71558fda60c9068c7dbbcf71
AureliusAtilius <j.j.h.lindner@gmail.com>
2024-01-18 09:44:33 -0800

    High temp job kill.

--- PowerChute Scripts/highTemp_JOBKILL.sh
+++ PowerChute Scripts/highTemp_JOBKILL.sh
@@ -4,7 +4,7 @@ t=$(date)
 
 if [ $job -gt 0 ] 
 then
-    echo "${t} High Temperature threshold exceeded." > ABORT
+    echo "${t} High Temperature threshold exceeded." > /projects/karem/starccm_projects/simInteraction/ABORT
 
 fi 
 
